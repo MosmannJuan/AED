8e76a28c9c9698a5612fba0386a342f817b088bb
MosmannJuan <63465251+MosmannJuan@users.noreply.github.com>
2020-04-19 15:20:42 -0300

    Create Hello.cpp

---
+++ 00-CppHelloWorld/Hello.cpp
@@ -0,0 +1,11 @@
+/*Tp 0 Hola, Mundo! 
+Mosmann, Juan Ignacio 
+AED curso K1051 año 2020*/
+#include <iostream>
+using namespace std; 
+int main () {
+    std::cout <<"Hola, Mundo! \n";
+
+    system("pause");
+    return 0;
+}
