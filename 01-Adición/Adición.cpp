9cc6bb8faff765a611033081c38f96f3cd07c3b3
MosmannJuan <63465251+MosmannJuan@users.noreply.github.com>
2020-04-29 14:28:51 -0300

    Create Adición.cpp

---
+++ 01-Adición/Adición.cpp
@@ -0,0 +1,22 @@
+/* Mosmann, Juan Ignacio
+TP N°1 Adición en C++
+26/04/2020
+Se deberá crear un programa que al ingresar dos valores de a y b devuelva la suma a+b */
+
+#include <iostream>
+
+int main() {
+int a;
+std::cout <<"Ingrese el valor de a: \n";
+std::cin >> a;
+int b;
+std::cout << "Ingrese el valor de b: \n";
+std::cin >> b;
+std::cout << "El resultado de la suma es: \n";
+std::cout << a + b;
+std::cout<< "\n" ;
+system ("pause");
+return 0;
+}
+
+
